a6198767c71b98f70f2444cb59faa2f727260ab6
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2015-11-03 16:58:41 -0600

    Initial commit

---
+++ ModuleFour/ModuleFour/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleFour
+{
+    class Program
+    {
+        public struct student
+        {
+            private string firstName;
+            private string lastName;
+            private DateTime birthDate;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
