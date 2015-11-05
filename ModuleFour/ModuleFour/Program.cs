b41271c9fb9370a0cf937fac9674c0d7d7789c09
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2015-11-05 12:10:09 -0600

    Hands-On Practice

--- ModuleFour/ModuleFour/Program.cs
+++ ModuleFour/ModuleFour/Program.cs
@@ -8,15 +8,49 @@ namespace ModuleFour
 {
     class Program
     {
-        public struct student
+        struct Student
         {
-            private string firstName;
-            private string lastName;
-            private DateTime birthDate;
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime BirthDate { get; set; }
+
+            public Student(string firstName, string lastName, DateTime birthDate)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                BirthDate = birthDate;
+            }
         }
+
+        enum Week
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+
+        enum Month
+        {
+            January, February, March, April, May, June, July, August, September, October, November, December
+        }
+
         static void Main(string[] args)
         {
-
+            Student student1 = new Student("Gerardo", "Martinez", new DateTime(1984, 5, 7));
+            Console.WriteLine("The student: {0} {1} was born on {2}", student1.FirstName, student1.LastName, student1.BirthDate.ToString());
+            string[] FiveArray = new string[5];
+            for (int i = 0; i < FiveArray.Length; i++)
+            {
+                FiveArray[i] = "arrValue" + i;
+            }
+            int j = 0;
+            while (j < FiveArray.Length)
+            {
+                Console.WriteLine(FiveArray[j]);
+                j++;
+            }
+            string[] WeekArray = {Week.Monday.ToString()};
+            Console.WriteLine("Using WeekAyyray: {0}", WeekArray[0]);
+            Console.WriteLine("Using Week: {0}", Week.Monday);
+            Console.ReadLine();
         }
     }
 }
